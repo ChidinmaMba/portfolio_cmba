d8b303e5cdddc5ca0c593281ad5eebafced0eaf2
ChidinmaMba <mbachidinma4@gmail.com>
2022-09-08 19:55:32 -0400

    removed warnings from App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavbarComp from './component/navigation/NavbarComp';
 
 function App() {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [username, setUsername] = useState();
-  const [password, setPassword] = useState();
-
   return (
     <div className="App">
         <div className="App">
