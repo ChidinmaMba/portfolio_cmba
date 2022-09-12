e5a1a8d3407be7904f0b99a3cb92227c14839347
ChidinmaMba <mbachidinma4@gmail.com>
2022-09-12 00:31:27 -0400

    removed todo functionality for blank home page. removed supporting files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,50 +1,10 @@
 import React, { Component } from 'react';
-import AddTodo from '../component/AddTodo';
-import Todos from "../component/todos.js";
 import "./Home.css";
   
 class Home extends Component {
-  // A default state of this component with an empty list of todos.
-  constructor() {
-    super();
-    this.state = {
-      // create your empty list here call it todos.
-      todos: []
-    };
-  }
-  // the addTodo function simply creates a new array that includes the user submitted todo item and then
-  // updates the state with the new list.
-  addTodo = (todo) => {
-    // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
-    // should not share duplicate values.
-    // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
-    // This solution works for a small application but a more complex hashing function should be used when
-    // dealing with a larger data sensitive project.
-    if (this.state.todos.map(x => x.content).includes(todo.content) || todo.due == null || todo.due === "Invalid Date"){
-      return;
-    } else{
-      todo.id = Math.random();
-      // An array that contains the current array and the new todo item
-      let new_list = [...this.state.todos, todo];
-      // Updates the local state with the new array.
-      this.setState({
-        todos: new_list,
-      });
-    }
-  };
-  deleteTodo = (id) => {
-    const todos = this.state.todos.filter((todo) => {
-      return todo.id !== id;
-    });
-    this.setState({
-      todos: todos,
-    });
-  };
   render() {
     return (
       <div className="Home">
-        <AddTodo addTodo={ this.addTodo } />
-        <Todos todos={ this.state.todos } deleteTodo={ this.deleteTodo } />
       </div>
     );
   }
