af40caf90f7aec726936371f2aa5fa70cddca743
ChidinmaMba <mbachidinma4@gmail.com>
2022-10-04 01:20:39 -0400

    changed hosting dir from public to build

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAzlPhDcjOtBseG1hSYxRmhijkee_QrNR0",
+  authDomain: "cmba-portfolio-blog.firebaseapp.com",
+  projectId: "cmba-portfolio-blog",
+  storageBucket: "cmba-portfolio-blog.appspot.com",
+  messagingSenderId: "759510103887",
+  appId: "1:759510103887:web:542fb023778303c0c4773a",
+  measurementId: "G-P8PCK2CTC0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,20 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
+        <div className="sidebar card">
+          <h3>Subjects</h3>
+          <hr></hr>
+          <ul>
+            <li>Coffee</li>
+            <li>Tea</li>
+            <li>Milk</li>
+          </ul>
+        </div>
+        <div className="content card">
+          <h3>Title</h3>
+          <hr></hr>
+          
+        </div>
       </div>
     );
   }
